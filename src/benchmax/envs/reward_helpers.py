6785ece25e37947e72e36f96b0bf03d27cca3bc7
NemiNate <finetune@cgft.io>
2026-06-11 01:16:20 -0700

    fix citation issue

--- src/benchmax/envs/reward_helpers.py
+++ src/benchmax/envs/reward_helpers.py
@@ -171,8 +171,6 @@ def citation_score(
 
     if not cited:
         return {"precision": 0.0, "recall": 0.0}
-    if not ref_ids:
-        return {"precision": 1.0, "recall": 0.0}
 
     precision = len(cited & ref_ids) / len(cited)
     recall = len(cited & ref_ids) / len(ref_ids)
